730f482ec3b076379f470ddacff5a419c3b13de4
root <root@ubuntu>
2025-03-15 13:54:30 +0000

    ajout docker privoxy proxy http

--- script/daily_check.sh
+++ script/daily_check.sh
@@ -6,4 +6,4 @@ Day=$(date --date='24 hours ago' '+%d-%m-%Y');day=$(date --date='24 hours ago' '
 echo "# Adresses bannies Fail2ban"
 Day=$(date --date='24 hours ago' '+%d-%m-%Y');day=$(date --date='24 hours ago' '+%Y-%m-%d'); cat /var/log/fail2ban.log | grep -E "^$day" | grep -e "NOTICE  \[sshd\] Ban" | awk -v day=$Day '{for(i=1;i<=NF;i++) if($i ~ /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/) print "address_ban_fail2ban;"day";"$i}'
 echo "# Adresses identifiées sur serveur web"
-day=$(date --date='24 hours ago' '+%d-%m-%Y'); cat /home/appli/log/logfile.log | grep $day | awk '{ print "address_identify_web_server;"$1";"$2}' 
+day=$(date --date='24 hours ago' '+%d-%m-%Y'); cat /home/ionos_server/docker_serveur_web_flask_myip/log/logfile.log | grep $day | awk '{ print "address_identify_web_server;"$1";"$2}' 
